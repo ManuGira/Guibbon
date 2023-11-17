e35341587483622f248d052eefcf94a8ea2b010c
Emmanuel Girardin <emmanuel.girardin@outlook.com>
2023-11-17 11:29:02 +0100

    ci: optimize dependencies installation

--- prepare_python.sh
+++ prepare_python.sh
@@ -13,8 +13,24 @@ rm -rf ./.venv
 rm -f poetry.lock
 poetry.exe install
 
-rm -f requirements_ci.txt
-poetry export --with dev --without-hashes -f requirements.txt --output requirements_ci.txt
-cat requirements_ci.txt
+# export requirements for tk4cv2
+rm -f requirements.txt
+poetry export --without-hashes -f requirements.txt --output requirements.txt
+cat requirements.txt
+
+# export requirements for ci linter and type checker
+rm -f requirements_ci_check.txt
+poetry export --with check --without-hashes -f requirements.txt --output requirements_ci_check.txt
+cat requirements_ci_check.txt
+
+# export requirements for ci testing
+rm -f requirements_ci_test.txt
+poetry export --with test --without-hashes -f requirements.txt --output requirements_ci_test.txt
+cat requirements_ci_test.txt
+
+# export requirements for developer
+rm -f requirements_dev.txt
+poetry export --with check,test --without-hashes -f requirements.txt --output requirements_dev.txt
+cat requirements_dev.txt
 
 popd
