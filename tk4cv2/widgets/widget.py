c0fe80aa5a2173010706e4547eff241ce7466afd
ManuGira <ManuGira@users.noreply.github.com>
2023-12-10 19:49:43 +0100

    feat: Fix typing for py 3.7

--- tk4cv2/widgets/widget.py
+++ tk4cv2/widgets/widget.py
@@ -1,9 +1,9 @@
-from typing import Any
+from typing import Any, Tuple
 import tkinter as tk
 import abc
 
 
 class WidgetInterface(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def __init__(self, master: tk.Frame, name: str, *params: tuple[Any]):
+    def __init__(self, master: tk.Frame, name: str, *params: Tuple[Any]):
         pass
